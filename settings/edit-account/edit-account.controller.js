234890782fc667454013957dfe6450d15c36089a
Paul Cioanca <pcioanca@smallfootprint.com>
2016-03-21 17:17:09 +0200

    Added bad option

--- settings/edit-account/edit-account.controller.js
+++ settings/edit-account/edit-account.controller.js
@@ -11,10 +11,22 @@
         var self = this;
 
         self.user = {};
+        self.passwordsMatch = true;
         self.submit = submit;
+        self.validatePasswordMatch = validatePasswordMatch;
+
+        function validatePasswordMatch() {
+            if (!self.user.password) {
+                self.passwordsMatch = true;
+            } else {
+                self.passwordsMatch = self.user.password === self.user.passwordConfirm;
+            }
+
+        }
+
 
         function submit() {
-            if (self.editAccountForm.$valid) {
+            if (self.editAccountForm.$valid && self.passwordsMatch) {
                 $rootScope.$broadcast('accountUpdate::successful', angular.copy(self.user));
             }
         }
